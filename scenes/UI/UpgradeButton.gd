b40338950996540cc1bb68bdf5de0f802d3178e0
Laurenz Reinthaler <schweinthaler@gmx.at>
2021-01-23 13:39:28 +0100

    Add basic Upgrade-UI

---
+++ scenes/UI/UpgradeButton.gd
@@ -0,0 +1,15 @@
+extends TextureButton
+
+func select() -> void:
+	modulate = Color(0.5, 1, 0, 1)
+	print("select")
+
+func deselect() -> void:
+	modulate = Color(1, 1, 1, 1 )
+	print("deselect")
+
+func _on_UpgradeButton_toggled(button_pressed : bool) -> void:
+	if button_pressed:
+		select()
+	else:
+		deselect()
