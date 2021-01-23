78abd8fbf4ba139a92984f670024667c10425e3f
Marek Maskarinec <fazolovy.kotlik@gmail.com>
2021-01-23 20:21:56 +0100

    added borders

--- scenes/dungeon/dungeon.gd
+++ scenes/dungeon/dungeon.gd
@@ -15,6 +15,7 @@ func _ready():
 			chunks[i].append(Generator.generate())
 			
 	draw()
+	add_frame()
 
 func draw():
 	var current
@@ -25,3 +26,11 @@ func draw():
 				for l in range(len(current[k])):
 					if current[k][l] == "wall":
 						$walls.set_cell(i*70+k, j*70+l, 0)
+						
+func add_frame():
+	for i in range(chunks_num.x*70):
+		for j in range(chunks_num.y*70):
+			if i == 0 or j == 0:
+				$walls.set_cell(i, j, 0)
+			if i == chunks_num.x*70-1 or j == chunks_num.y*70-1:
+				$walls.set_cell(i, j, 0)
