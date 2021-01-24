4bbf9940440fd44207b7b0cd61c9c35c7d28511a
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-24 19:32:53 +0000

    Merge temp_level into main

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,8 +2,25 @@ extends Node2D
 
 var bullet: PackedScene = preload("res://scenes/entities/player/bullet.tscn")
 
+onready var nav_2d: Navigation2D = $Navigation2D
+onready var line_2d: Line2D = $Line2D
+onready var enemy: KinematicBody2D = $Enemy
+onready var player: KinematicBody2D = $Player
 onready var shoot_pos: Position2D = $Player/Gun/ShootPos
 onready var gun: Sprite = $Player/Gun
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("fire"):
+		shoot()
+	
+	if not event is InputEventMouseButton or event.button_index != BUTTON_LEFT or not event.pressed:
+		return
+
+	var new_path := nav_2d.get_simple_path(enemy.global_position, player.global_position, false)
+	line_2d.points = new_path
+	new_path.remove(0)
+	enemy.path = new_path
 
 
 func shoot() -> void:
@@ -12,8 +29,3 @@ func shoot() -> void:
 	bullet_instance.rotation_degrees = gun.rotation_degrees
 	bullet_instance.vector = Vector2(1, 0).rotated(gun.rotation)
 	add_child(bullet_instance)
-
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("fire"):
-		shoot()
