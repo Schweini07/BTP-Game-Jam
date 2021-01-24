1916fb8735be9d194eb9703f2491091552cc003a
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-24 01:35:34 +0000

    Add WIP enemy AI

---
+++ scenes/entities/enemies/enemy.gd
@@ -0,0 +1,36 @@
+extends "res://scenes/entities/base_entity/base_entity.gd"
+
+const SPEED := 300
+
+var path: PoolVector2Array setget set_path
+
+var _start_point := Vector2.ZERO
+
+
+func _ready() -> void:
+	set_physics_process(false)
+
+
+func _physics_process(delta: float) -> void:
+	_move_along_path()
+	
+	if position.distance_to(path[0]) <= SPEED * delta:
+		_velocity = Vector2.ZERO
+		path.remove(0)
+		_start_point = position
+	if not path:
+		set_physics_process(false)
+
+
+func set_path(value: PoolVector2Array) -> void:
+	path = value
+	if not value:
+		return
+	set_physics_process(true)
+	_start_point = position
+
+
+func _move_along_path() -> void:
+	var next_point := path[0]
+	var dir = position.direction_to(next_point)
+	_velocity = dir * SPEED
---
+++ scenes/temp_level/temp_level.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+onready var nav_2d: Navigation2D = $Navigation2D
+onready var line_2d: Line2D = $Line2D
+onready var enemy: KinematicBody2D = $Enemy
+onready var player: KinematicBody2D = $Player
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not event is InputEventMouseButton or event.button_index != BUTTON_LEFT or not event.pressed:
+		return
+
+	var new_path := nav_2d.get_simple_path(enemy.global_position, player.global_position)
+	line_2d.points = new_path
+	new_path.remove(0)
+	enemy.path = new_path
