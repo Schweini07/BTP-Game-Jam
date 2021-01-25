79244a46b42c45b3cfb07593773e8e4bbdf03d06
Marek Maskarinec <fazolovy.kotlik@gmail.com>
2021-01-25 21:04:15 +0100

    remove printing

--- scenes/dungeon/doors/doors.gd
+++ scenes/dungeon/doors/doors.gd
@@ -3,11 +3,9 @@ extends Node2D
 onready var player = get_node("../../Player")
 onready var tilemap = get_node("../../Navigation2D/TileMap")
 onready var pos = Vector2(int(position.x/32), int(position.y/32))
-var def_overlap
 
 func _ready():
 	tilemap.set_cell(pos.x, pos.y, 5)
-	def_overlap = len($Area2D.get_overlapping_bodies())
 
 func _on_Area2D_body_entered(body):
 	if body == player:
@@ -15,7 +13,6 @@ func _on_Area2D_body_entered(body):
 
 
 func _on_Area2D_body_exited(body):
-	print(len($Area2D.get_overlapping_bodies()))
 	if body == player:
 		if 1 >= len($Area2D.get_overlapping_bodies()):
 			tilemap.set_cell(pos.x, pos.y, 5)
