c99dd28414b927c79cea9144854b944cdcf5c976
Laurenz Reinthaler <schweinthaler@gmx.at>
2021-01-25 15:25:04 +0100

    Small bullet improvements

--- scenes/entities/player/bullet.gd
+++ scenes/entities/player/bullet.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var speed = 1000
+var speed = 500
 var vector: Vector2
 
 onready var anim: AnimationPlayer = $spr/anim
@@ -10,8 +10,8 @@ func _physics_process(delta) -> void:
 	position += vector * speed * delta
 
 func explode() -> void:
-	coll.disabled = true
 	speed = 0
+	coll.call_deferred("set", "disabled", true)
 	yield(get_tree().create_timer(1), "timeout")
 	call_deferred("queue_free")
 
@@ -20,6 +20,7 @@ func _on_SelfDestroy_timeout() -> void:
 
 func _on_Bullet_body_entered(body) -> void:
 	if body is TileMap:
+		explode()
 		$spr/anim.play("Explode")
 	elif body is KinematicBody2D:
 		print("enemy hit")
