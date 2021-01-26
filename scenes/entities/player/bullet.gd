6fe0fa7b938ae96d390448b0c7a1caf98edf697d
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-26 02:16:13 +0000

    Add damaging and killing enemies

--- scenes/entities/player/bullet.gd
+++ scenes/entities/player/bullet.gd
@@ -1,13 +1,17 @@
 extends Area2D
 
+const BULLET_DAMAGE := 10.0
+
 var speed = 500
 var vector: Vector2
 
 onready var anim: AnimationPlayer = $spr/anim
 onready var coll: CollisionShape2D = $coll
 
+
 func _physics_process(delta) -> void:
 	position += vector * speed * delta
+
 
 func explode() -> void:
 	speed = 0
@@ -15,12 +19,15 @@ func explode() -> void:
 	yield(get_tree().create_timer(1), "timeout")
 	call_deferred("queue_free")
 
+
 func _on_SelfDestroy_timeout() -> void:
 	$spr/anim.play("Explode")
 
-func _on_Bullet_body_entered(body) -> void:
-	if body is TileMap:
-		explode()
-		$spr/anim.play("Explode")
-	elif body is KinematicBody2D:
-		print("enemy hit")
+
+func _on_Bullet_body_entered(_body: Node2D) -> void:
+	explode()
+	$spr/anim.play("Explode")
+
+
+func _on_Bullet_area_entered(area: Area2D) -> void:
+	area.emit_signal("hitbox_activated", BULLET_DAMAGE)
