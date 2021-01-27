2eaef43db37673fea48383128644b9c3edf3cb76
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-27 10:00:27 +0000

    Fix #35

--- scenes/dungeon/doors/doors.gd
+++ scenes/dungeon/doors/doors.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var tilemap = get_node("../../Navigation2D/TileMap")
 onready var pos = Vector2(int(position.x/32), int(position.y/32))
+onready var area = $Area2D
 
 func _ready():
 	tilemap.set_cell(pos.x, pos.y, 5)
@@ -12,8 +13,7 @@ func _on_Area2D_body_entered(body):
 
 
 func _on_Area2D_body_exited(body):
-	if body.is_in_group("player"):
-		if 1 >= len($Area2D.get_overlapping_bodies()):
-			tilemap.set_cell(pos.x, pos.y, 5)
-	else:
-		tilemap.set_cell(pos.x, pos.y, 5)
+	if len(area.get_overlapping_bodies()) > 1:
+		return
+	
+	tilemap.set_cell(pos.x, pos.y, 5)
