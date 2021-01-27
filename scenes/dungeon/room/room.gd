97cc22b671cf49368ddfa12e242a130006bad92c
Marek Maskarinec <fazolovy.kotlik@gmail.com>
2021-01-27 09:57:31 +0100

    add enemy spawning

--- scenes/dungeon/room/room.gd
+++ scenes/dungeon/room/room.gd
@@ -3,11 +3,22 @@ extends Node2D
 var wh
 var box_chance = 6
 var boxes = [load("res://scenes/dungeon/room/boxes/box.tscn")]
-
+var boss_multiplicator = 1
 
 func _ready():
 	add_boxes()
+	add_enemies()
 
+func add_enemies():
+	var enemy_num = (randi()%4+1) * boss_multiplicator 
+	var inst
+	for _i in range(enemy_num):
+		inst = load("res://scenes/entities/enemies/base_enemy.tscn").instance()
+		inst.position = Vector2(
+			randi()%int(wh.x-2)*32+48,
+			randi()%int(wh.y-2)*32+48
+		)
+		$enemies.add_child(inst)
 
 func add_boxes():
 	var box_num = wh.x * wh.y * (0.01 * box_chance)
