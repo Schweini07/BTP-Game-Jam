add24ab05f5b06d10dea353c1fdd97f803c97af7
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-28 21:16:04 +0000

    Reduce path calculations

--- scenes/entities/enemies/ai/states/state_disabled.gd
+++ scenes/entities/enemies/ai/states/state_disabled.gd
@@ -1,5 +1,9 @@
 extends AIState
 
 
+func post_initialize() -> void:
+	enemy.remove_from_group("enemies_attacking")
+
+
 func execute(_delta: float, _path_to_player: PoolVector2Array, _nearby_enemies: Array) -> void:
 	return
