ed816130ab892fea806f5af4d22fb39ad68e5674
Marek Maskarinec <fazolovy.kotlik@gmail.com>
2021-01-29 11:11:46 +0100

    fix gun flinching

--- scenes/entities/player/gun.gd
+++ scenes/entities/player/gun.gd
@@ -3,9 +3,9 @@ extends Sprite
 var positions = [Vector2(2, 7.6), Vector2(-2, 7.6)]
 
 func _process(_delta):
-	if abs(int(rotation_degrees)%360) > 90 and abs(int(rotation_degrees)%360) < 270:
+	if abs(int(rotation_degrees)%360) > 95 and abs(int(rotation_degrees)%360) < 265:
 		flip_v = true
 		position = positions[1]
-	else:
+	if abs(int(rotation_degrees)%360) < 85 or abs(int(rotation_degrees)%360) > 275:
 		flip_v = false
 		position = positions[0]
