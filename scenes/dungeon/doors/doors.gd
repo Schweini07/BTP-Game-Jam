228eaa963b7730dce620dfc3d1ab96dcb4b0105e
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-29 13:28:58 +0000

    Add door open and close SFX

--- scenes/dungeon/doors/doors.gd
+++ scenes/dungeon/doors/doors.gd
@@ -3,11 +3,15 @@ extends Node2D
 onready var tilemap = get_node("../../Navigation2D/TileMap")
 onready var pos = Vector2(int(position.x/32), int(position.y/32))
 onready var area = $Area2D
+onready var open_sfx: AudioStreamPlayer2D = $OpenSFX
+onready var close_sfx: AudioStreamPlayer2D = $CloseSFX
 
 func _ready():
 	tilemap.set_cell(pos.x, pos.y, 5)
 
 func _on_Area2D_body_entered(body):
+	open_sfx.pitch_scale = rand_range(0.9, 1.1)
+	open_sfx.play()
 	tilemap.set_cell(pos.x, pos.y, 4)
 
 
@@ -15,4 +19,6 @@ func _on_Area2D_body_exited(body):
 	if len(area.get_overlapping_bodies()) > 1:
 		return
 	
+	close_sfx.pitch_scale = rand_range(0.9, 1.1)
+	close_sfx.play()
 	tilemap.set_cell(pos.x, pos.y, 5)
